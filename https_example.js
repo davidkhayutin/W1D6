bd3762726d42685f0406291f098d670c35af19e6
David <david.khayutin@gmail.com>
2018-09-29 23:24:54 +0000

    second commit

--- https_example.js
+++ https_example.js
@@ -1,3 +1,18 @@
 var http = require('https');
 
-console.log("I Did it!");
+
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+var callback = function(){
+  console.log('In response handler callback!');
+}
+
+console.log("Im about to make the request");
+
+http.request(options, callback).end();
+
+
+console.log("I've made the request!");
